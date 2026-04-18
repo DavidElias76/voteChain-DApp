87c4d3be1a1b34556b1e7281472428590bc98fa9
David <eliaskimutai76@gmail.com>
2026-04-18 19:19:16 +0300

    updated some files

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 // Security middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  origin: process.env.CLIENT_URL || "http://localhost:5173", // change the origin when deployment - add the new url when deployinng to connect to the frontend
   credentials: true
 }));
 
